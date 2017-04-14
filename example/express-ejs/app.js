729259ffc29502f0b9ea752aa990e999a56d151c
Marco Bruno <marco.bruno.br@gmail.com>
2017-04-14 18:20:12 -0300

    Implemented a simples example with express and ejs

--- example/express-ejs/app.js
+++ example/express-ejs/app.js
@@ -1,12 +1,19 @@
-const expresss = require('express');
-const app = expresss();
-const port = 3000;
+'use strict';
 
-app.set('view engine', 'ejs')
+const path = require('path')
+    , express = require('express')
+    , app = express()
+    , port = 3000
+    , assets = require('../../index')
+    , assetPath = path.join(__dirname, 'public');
 
-app.get('/', (req, res) => res.render('index'))
+app.use('/public', express.static(assetPath));
+app.use(assets('/public', assetPath));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => res.render('index'));
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`)
-  console.log('To down the server: ctrl + c')
-})
+  console.log(`Server running at http://localhost:${port}`);
+  console.log('To down the server: ctrl + c');
+});
